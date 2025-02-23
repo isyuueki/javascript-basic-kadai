92729c5ffafd60a120fcff3952e73905f94ca0f9
isyuueki <isyuueki@gmail.com>
2025-02-23 13:58:31 +0800

    kadai_mdn_js再提出

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,9 +1,7 @@
-let date = new Date(2025, 1, 17)
-const options = {
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-};
-console.log(date.toLocaleDateString('ja-jp', options))
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDate();
+const result = year + "年" + month + "月" + day + "日";
 
-
+console.log(result);
