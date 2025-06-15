21e3ee5b6a52f6d8545be2645296b8a7d2688f86
isyuueki <isyuueki@gmail.com>
2025-06-15 17:33:14 +0800

    kadai_mdn_js再提出

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
 const date = new Date();
 const year = date.getFullYear();
-const month = date.getMonth();
+const month = date.getMonth() + 1;
 const day = date.getDate();
 const result = year + "年" + month + "月" + day + "日";
 
